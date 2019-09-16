a13ee5430e2d6bd95f5e9045dd612af69f319c44
earlbingham <ebingham@gmail.com>
2019-09-16 15:08:44 -0700

    added README file and a couple of tests

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a repository to test out IntelliJ features that work with Github.
+
+<h1> Examples</h1>
+
+<p> Use of Optional and how it can be used. </p>
--- src/main/java/com/example/test/OptionalTest.java
+++ src/main/java/com/example/test/OptionalTest.java
@@ -1,19 +1,36 @@
 package com.example.test;
 
+import org.junit.Test;
+
 import java.util.Optional;
 
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 public class OptionalTest {
-    private static Object testString;
     // To try out optional and its usefulness
 
-    Optional<String> testString = Optional.empty();
+    static Optional<String> testString = Optional.empty();
 
     public OptionalTest() {
     }
 
+    @Test
+    public void whenCreatesEmptyOptional_thenCorrect() {
+        Optional<String> empty = Optional.empty();
+        assertFalse(empty.isPresent());
+    }
+
+    @Test
+    public void givenNonNull_whenCreatesNonNullable_thenCorrect() {
+        String name = "baeldung";
+        Optional<String> opt = Optional.of(name);
+        assertTrue(opt.isPresent());
+    }
+
     public static void main(String[] args) {
         System.out.println("Test optional");
-        if(testString != null) {
+        if(testString.isPresent()) {
             System.out.println("what");
         }
     }
